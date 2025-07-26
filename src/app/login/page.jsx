bb8f1a16a0a1a1616306001de134f845508e3e89
Ephymeral Aduare <109929578+rwudboy@users.noreply.github.com>
2025-07-26 16:49:53 +0700

    update icon

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -8,10 +8,8 @@ export default function LoginPage() {
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
           <a href="/login" className="flex items-center gap-2 font-medium">
-            <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-              <GalleryVerticalEnd className="size-4" />
-            </div>
-           Audit Trail Management System
+            <img src="/icon.jpg" alt="Logo" className="!size-5 object-cover rounded" />
+            Audit Trail Management System
           </a>
         </div>
         <div className="flex flex-1 items-center justify-center">
--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -7,11 +7,9 @@ export default function RegisterPage() {
     <div className="grid min-h-svh lg:grid-cols-2">
       <div className="flex flex-col gap-4 p-6 md:p-10">
         <div className="flex justify-center gap-2 md:justify-start">
-          <a href="/" className="flex items-center gap-2 font-medium">
-            <div className="flex h-6 w-6 items-center justify-center rounded-md bg-primary text-primary-foreground">
-              <GalleryVerticalEnd className="size-4" />
-            </div>
-           Audit Trail Management System
+              <a href="/login" className="flex items-center gap-2 font-medium">
+            <img src="/icon.jpg" alt="Logo" className="!size-5 object-cover rounded" />
+            Audit Trail Management System
           </a>
         </div>
         <div className="flex flex-1 items-center justify-center">
